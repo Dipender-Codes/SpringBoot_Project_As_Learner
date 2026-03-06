ed0c9f0e8d77a9b1243361284ff7bd33b7b14933
Dipender-Codes <mdipenderrajpandey22@gmail.com>
2026-03-06 11:37:50 +1100

    Added Lombok dependency and enable annotation processing

--- src/main/java/com/example/journalApplication/entity/JournalEntry.java
+++ src/main/java/com/example/journalApplication/entity/JournalEntry.java
@@ -1,36 +1,38 @@
 package com.example.journalApplication.entity;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "journalDB")
+@Data
 public class JournalEntry {
     @Id
     private long id;
     private String title;
     private String content;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
+//    public long getId() {
+//        return id;
+//    }
+//
+//    public void setId(long id) {
+//        this.id = id;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getContent() {
+//        return content;
+//    }
+//
+//    public void setContent(String content) {
+//        this.content = content;
+//    }
 }
