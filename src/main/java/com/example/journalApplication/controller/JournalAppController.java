e76f6b7207c801629e080af96c064a7a29465970
Dipender-Codes <mdipenderrajpandey22@gmail.com>
2026-03-06 11:14:49 +1100

    Updated CRUD operations to return ResponseEntity with proper HTTP status codes

--- src/main/java/com/example/journalApplication/controller/JournalAppController.java
+++ src/main/java/com/example/journalApplication/controller/JournalAppController.java
@@ -3,8 +3,11 @@ package com.example.journalApplication.controller;
 import com.example.journalApplication.entity.JournalEntry;
 import com.example.journalApplication.service.JournalAppService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.print.attribute.standard.JobHoldUntil;
 import java.util.*;
 
 @RestController
@@ -12,37 +15,64 @@ import java.util.*;
 public class JournalAppController {
     @Autowired
     private JournalAppService journalAppService;
-@PostMapping
-public boolean createEntry(@RequestBody JournalEntry myEntry){
-    journalAppService.saveEntryToTheDatabase(myEntry);
-    return true;
-}
 
-@GetMapping
-    public List<JournalEntry> getAllEntries(){
-    return journalAppService.getAllEntries();
-}
-
-@GetMapping("id/{myId}")
-    public Optional<JournalEntry> getEntryByid(@PathVariable Long myId){
-    return journalAppService.getEntryId(myId);
-}
-
-@DeleteMapping("id/{myId}")
-    public void deleteEntryById(@PathVariable Long myId){
-    journalAppService.deleteEntryById(myId);
-}
-
-
-@PutMapping("id/{myId}")
-    public JournalEntry updateById(@PathVariable Long myId, @RequestBody JournalEntry myjournal) {
-    JournalEntry oldEntry = journalAppService.getEntryId(myId).orElse(null);
-    if (oldEntry != null && !myjournal.getTitle().isEmpty()) {
-        oldEntry.setTitle(myjournal.getTitle());
+    @PostMapping
+    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry) {
+        try {
+            journalAppService.saveEntryToTheDatabase(myEntry);
+            return new ResponseEntity<JournalEntry>(HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
-    if (oldEntry != null && !myjournal.getContent().isEmpty()) {
-    oldEntry.setContent(myjournal.getContent());
+
+    @GetMapping
+    public ResponseEntity<List<JournalEntry>> getAllEntries() {
+        List<JournalEntry> listInDatabase = journalAppService.getAllEntries();
+        if (listInDatabase != null && !listInDatabase.isEmpty()) {
+            return new ResponseEntity<>(listInDatabase, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
     }
-    return journalAppService.saveEntryToTheDatabase(oldEntry);
+
+    @GetMapping("id/{myId}")
+    public ResponseEntity<JournalEntry> getEntryByid(@PathVariable Long myId) {
+        Optional<JournalEntry> myjournal = journalAppService.getEntryId(myId);
+        if (myjournal.isPresent() && myjournal != null) {
+            return new ResponseEntity<>(myjournal.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+    }
+
+    @DeleteMapping("id/{myId}")
+    public ResponseEntity<JournalEntry> deleteEntryById(@PathVariable Long myId) {
+        Optional<JournalEntry> myjournal = journalAppService.getEntryId(myId);
+        if (myjournal.isPresent()) {
+            journalAppService.deleteEntryById(myId);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+
+    @PutMapping("id/{myId}")
+    public ResponseEntity<JournalEntry> updateById(@PathVariable Long myId, @RequestBody JournalEntry myjournal) {
+        JournalEntry oldEntry = journalAppService.getEntryId(myId).orElse(null);
+        if (oldEntry != null) {
+            if (myjournal.getTitle() != null && !myjournal.getTitle().isEmpty()) {
+                oldEntry.setTitle(myjournal.getTitle());
+            }
+            if (myjournal.getContent() != null && !myjournal.getContent().isEmpty()) {
+                oldEntry.setContent(myjournal.getContent());
+            }
+            journalAppService.saveEntryToTheDatabase(oldEntry);
+            return new ResponseEntity<>(oldEntry, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
-}
+
